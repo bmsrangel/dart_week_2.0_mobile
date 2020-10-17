48c5f745d9014591cc8ce7cddb09d6d954a0b9f7
bmsrangel <bmsrangel@hotmail.com>
2020-10-17 17:41:52 -0300

    Changed constants.dart to be easier to set the server address

--- lib/app/shared/utils/constants.dart
+++ lib/app/shared/utils/constants.dart
@@ -1,1 +1,8 @@
-const String baseUrl = "http://192.168.0.9:8888";
+// First set the server's IP Address
+const String serverIp = "192.168.0.9";
+
+// Then set the server's port
+const String serverPort = "8888";
+
+// For last, just check if it's either "http" or "https"
+const String baseUrl = "http://$serverIp:$serverPort";
