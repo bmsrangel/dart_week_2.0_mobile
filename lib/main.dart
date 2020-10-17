aa71295641040c0bc078d928ef9b518f9324944c
bmsrangel <bmsrangel@hotmail.com>
2020-10-17 18:30:28 -0300

    Changed app name in AndroidManifest and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pizza Delivery App',
       theme: ThemeData(
         primaryColor: Color(0xFF9D0000),
         primarySwatch: Colors.red,
